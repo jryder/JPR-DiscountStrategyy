5d674f1de1bb85232d6411106e3edf2918b006c3
jorda_000 <jorda_000@192.168.0.102>
2013-02-24 20:09:06 -0600

    working...

--- src/jpr/discountstrategy/Product.java
+++ src/jpr/discountstrategy/Product.java
@@ -9,52 +9,53 @@ package jpr.discountstrategy;
  * @author jryder
  */
 public class Product {
+
     private int productId;
     private String name;
     private double unitSell;
     DiscountStrategy discountStrategy;
+ 
+    public Product(int prod, String nam, double sell, DiscountStrategy discountStrategy) {
 
-    public Product(int prod, String nam, double sell, DiscountStrategy discountStrategy){
-    
-        productId = prod;
-        name = nam;
-        unitSell = sell;
-        this.discountStrategy = discountStrategy;
+	productId = prod;
+	name = nam;
+	unitSell = sell;
+	this.discountStrategy = discountStrategy;
     }
 
     public int getProductId() {
-        return productId;
+	return productId;
     }
 
     public void setProductId(int productId) {
-        this.productId = productId;
+	this.productId = productId;
     }
 
     public DiscountStrategy getDiscountStrategy() {
-        return discountStrategy;
+	return discountStrategy;
     }
 
     public void setDiscountStrategy(DiscountStrategy discountStrategy) {
-        this.discountStrategy = discountStrategy;
+	this.discountStrategy = discountStrategy;
     }
-    
+
     public String getName() {
-        return name;
+	return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+	this.name = name;
     }
 
     public double getUnitSell() {
-        return unitSell;
+	return unitSell;
     }
 
     public void setUnitSell(double unitSell) {
-        this.unitSell = unitSell;
+	this.unitSell = unitSell;
     }
 
     public static void main(String[] args) {
-        Product p = new Product(123,"nice product",50.00,new FlatAmountDiscount());
+	Product p = new Product(123, "nice product", 50.00, new FlatAmountDiscount());
     }
 }
